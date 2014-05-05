a02bee4a14c2050c0921623b37d7a4586c593517
sudodoki <smd.deluzion@gmail.com>
2014-05-06 01:00:29 +0300

    Add dev server - livereload adder

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,23 @@ module.exports = function (grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          open: 'http://0.0.0.0:8080/develop.html',
+          port: 8080,
+          livereload: 35729
+        }
+      }
+    },
     watch: {
       dev: {
+        files: ['**/*', '!node_modules/**', '!lib/**'],
+        options: {
+          livereload: true
+        }
+      },
+      rebuild: {
         files: ['**/*', '!node_modules/**', '!lib/**'],
         tasks: ['default'],
         options: {
